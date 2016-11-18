75631e6da4441cf15f3c4e9b8e882cf57550ec6b
puregardenia@qq.com <puregardenia@qq.com>
2016-11-18 20:58:10 +0800

    完成前六课

--- lessons/05-active-links/modules/App.js
+++ lessons/05-active-links/modules/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import NavLink from './NavLink'
+
 export default React.createClass({
   render() {
     return (
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
+          {/*<li><Link to="/about" activeStyle={{color: 'red'}}>About</Link></li>
+          <li><Link to="/repos" activeStyle={{color: 'red'}}>Repos</Link></li>*/}
+
+          {/* index.html 中增加 index.css */}
+          {/*<li><Link to="/about" activeClassName="active">About</Link></li>
+          <li><Link to="/repos" activeClassName="active">Repos</Link></li>*/}
+
+          {/* 使用自定义的Link 组件 */}
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+
         </ul>
         {this.props.children}
       </div>
